9d2a50fe5ed580265c33659d4772d3822ae2f4c4
cle5eland <clthomps@microsoft.com>
2018-10-11 16:46:28 -0700

    Resolving Testing Environment

---
+++ src/ResourceManager/IotCentral/Commands.IotCentral.Test/ScenarioTests/IotCentralController.cs
@@ -0,0 +1,170 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Management.Internal.Resources;
+
+namespace Microsoft.Azure.Commands.IotCentral.Test.ScenarioTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using Microsoft.Azure.Commands.Common.Authentication;
+    using Microsoft.Azure.Gallery;
+    using Microsoft.Azure.Management.Authorization;
+    using Microsoft.Azure.Subscriptions;
+    using Microsoft.Azure.Test;
+    using Microsoft.Azure.Test.HttpRecorder;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using Microsoft.WindowsAzure.Commands.ScenarioTest;
+    using TestBase = Microsoft.Azure.Test.TestBase;
+    using TestUtilities = Microsoft.Azure.Test.TestUtilities;
+    using ServiceManagemenet.Common.Models;
+    using Microsoft.Azure.Management.IotCentral;
+    using TestEnvironmentFactory = Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
+
+    public sealed class IotCentralController
+    {
+        private EnvironmentSetupHelper _helper;
+
+        public ResourceManagementClient ResourceManagementClient { get; private set; }
+
+        public SubscriptionClient SubscriptionClient { get; private set; }
+
+        public GalleryClient GalleryClient { get; private set; }
+
+        public AuthorizationManagementClient AuthorizationManagementClient { get; private set; }
+
+        public IotCentralClient IotCentralClient { get; private set; }
+
+        public string UserDomain { get; private set; }
+
+        public static IotCentralController NewInstance
+        {
+            get
+            {
+                return new IotCentralController();
+            }
+        }
+
+        public IotCentralController()
+        {
+            _helper = new EnvironmentSetupHelper();
+        }
+
+        public void RunPsTest(XunitTracingInterceptor logger, params string[] scripts)
+        {
+            var callingClassType = TestUtilities.GetCallingClass(2);
+            var mockName = TestUtilities.GetCurrentMethodName(2);
+
+            RunPsTestWorkflow(
+                logger,
+                () => scripts,
+                // no custom initializer
+                null,
+                // no custom cleanup
+                null,
+                callingClassType,
+                mockName);
+        }
+
+
+        public void RunPsTestWorkflow(
+            XunitTracingInterceptor logger,
+            Func<string[]> scriptBuilder,
+            Action<CSMTestEnvironmentFactory> initialize,
+            Action cleanup,
+            string callingClassType,
+            string mockName)
+        {
+            _helper.TracingInterceptor = logger;
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            d.Add("Microsoft.Resources", null);
+            d.Add("Microsoft.Features", null);
+            d.Add("Microsoft.Authorization", null);
+            var providersToIgnore = new Dictionary<string, string>();
+            providersToIgnore.Add("Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01");
+            HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, d, providersToIgnore);
+            HttpMockServer.RecordsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SessionRecords");
+
+            using (MockContext context = MockContext.Start(callingClassType, mockName))
+            {
+                this._helper.SetupEnvironment(AzureModule.AzureResourceManager);
+
+                SetupManagementClients(context);
+                var callingClassName = callingClassType
+                                        .Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
+                                        .Last();
+                this._helper.SetupModules(AzureModule.AzureResourceManager,
+                    "ScenarioTests\\Common.ps1",
+                    "ScenarioTests\\" + callingClassName + ".ps1",
+                    this._helper.RMProfileModule,
+                    this._helper.RMResourceModule,
+                    this._helper.RMInsightsModule);
+                try
+                {
+                    if (scriptBuilder != null)
+                    {
+                        var psScripts = scriptBuilder();
+
+                        if (psScripts != null)
+                        {
+                            this._helper.RunPowerShellTest(psScripts);
+                        }
+                    }
+                }
+                finally
+                {
+                    cleanup?.Invoke();
+                }
+            }
+        }
+
+        private void SetupManagementClients(MockContext context)
+        {
+            this.ResourceManagementClient = GetResourceManagementClient(context);
+            this.SubscriptionClient = GetSubscriptionClient(context);
+            this.IotCentralClient = GetIotCentralClient(context);
+            this.AuthorizationManagementClient = GetAuthorizationManagementClient(context);
+
+            this._helper.SetupManagementClients(this.ResourceManagementClient,
+                this.SubscriptionClient,
+                this.IotCentralClient,
+                this.AuthorizationManagementClient
+                );
+        }
+
+        private AuthorizationManagementClient GetAuthorizationManagementClient(MockContext context)
+        {
+            return context.GetServiceClient<AuthorizationManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
+        }
+
+        private ResourceManagementClient GetResourceManagementClient(MockContext context)
+        {
+            return context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
+        }
+
+        private SubscriptionClient GetSubscriptionClient(MockContext context)
+        {
+            return context.GetServiceClient<SubscriptionClient>(TestEnvironmentFactory.GetTestEnvironment());
+        }
+
+        private IotCentralClient GetIotCentralClient(MockContext context)
+        {
+            return context.GetServiceClient<IotCentralClient>(TestEnvironmentFactory.GetTestEnvironment());
+        }
+    }
+}
---
+++ src/ResourceManager/IotCentral/Commands.IotCentral.Test/ScenarioTests/IotCentralTests.ps1
@@ -0,0 +1,185 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+
+#################################
+## IotCentral Cmdlets			   ##
+#################################
+
+$global:resourceType = "Microsoft.IoTCentral/IotApps"
+
+<#
+.SYNOPSIS
+Tests creating Resource Group and Iot Central App
+#>
+
+function Test-CreateSimpleIotCentralApp{
+	# Setup
+	$rgname = Get-ResourceGroupName
+	$rname = Get-ResourceName
+	$location = Get-Location "Microsoft.IoTCentral" "IotApps" 
+	
+	try
+	{
+		# Test
+
+		# Create Resource Group
+		New-AzureRmResourceGroup -Name $rgname -Location $rglocation
+
+		# Create App
+		New-AzureRmIotCentralApp -ResourceGroupName $rgname -Name $rname -Subdomain $rname
+		$actual = Get-AzureIotCentralApp -ResourceGroupName $rgname -Name $rname
+
+		$list = Get-AzureRmIotCentralApp -ResourceGroupName $rgname
+	
+		# Assert
+
+		# Name
+		Assert-AreEqual $actual.Name $rname
+		Assert-AreEqual $expected.ResourceGroupName $actual.ResourceGroupName
+		Assert-AreEqual $actual.Subdomain $rname
+		Assert-AreEqual $actual.ResourceType $resourceType
+		Assert-AreEqual 1 @($list).Count
+		Assert-AreEqual $actual.Name $list[0].Name
+	}
+	finally{
+		# Clean up
+		Clean_ResourceGroup $rgname
+	}
+}
+
+function Test-CreateComplexIotCentralApp{
+	# Setup
+	$rgname = Get-ResourceGroupName
+	$rname = Get-ResourceName
+	$subdomain = ($rname) + "subdomain"
+	$location = Get-Location "Microsoft.IoTCentral" "IotApps"
+	$sku = "S1"
+	$displayName = "Custom IoT Central App DisplayName"
+	$tagKey = "key1"
+	$tagValue = "value1"
+	$tags = @{ $tagKey = $tagValue }
+	
+	try
+	{
+		# Test
+
+		# Create Resource Group
+		New-AzureRmResourceGroup -Name $rgname -Location $rglocation
+
+		# Create App
+		New-AzureRmIotCentralApp -ResourceGroupName $rgname -Name $rname -Subdomain $subdomain -Sku $sku -DisplayName $displayName -Tag $tags
+		$actual = Get-AzureIotCentralApp -ResourceGroupName $rgname -Name $rname
+
+		$list = Get-AzureRmIotCentralApp -ResourceGroupName $rgname
+	
+		# Assert
+
+		# Name
+		Assert-AreEqual $actual.Name $rname
+		Assert-AreEqual $expected.ResourceGroupName $actual.ResourceGroupName
+		Assert-AreEqual $actual.Subdomain $subdomain
+		Assert-AreEqual $actual.Sku $sku
+		Assert-AreEqual $actual.DisplayName $displayName
+		Assert-AreEqual $actual.Tag.Item($tagkey) $tagvalue
+		Assert-AreEqual $actual.ResourceType $resourceType
+		Assert-AreEqual 1 @($list).Count
+		Assert-AreEqual $actual.Name $list[0].Name
+	}
+	finally{
+		# Clean up
+		Clean_ResourceGroup $rgname
+	}
+}
+
+<#
+.SYNOPSIS
+Test Iot Central Application Lifecycle PowerShell cmdlets
+#>
+function Test-AzureRmIotCentralAppLifecycle
+{
+	$Location = Get-Location "Microsoft.IoTCentral" "IotApps" 
+	$IotCentralAppName = getAssetName
+	$IotCentralSubdomain = getAssetName
+	$ResourceGroupName = getAssetName 
+	$Sku = "S1"
+	$DisplayName = "Custom IoT Central App DisplayName"
+	$Tag1Key = "key1"
+	$Tag2Key = "key2"
+	$Tag1Value = "value1"
+	$Tag2Value = "value2"
+
+	# Get all Iot Central Apps in the subscription
+	$allIotCentralApps = Get-AzureRmIotCentralApp
+
+	Assert-True { $allIotCentralApps[0].Type -eq $global:resourceType }
+	Assert-True { $allIotCentralApps.Count -gt 1 }
+
+	# Create or Update Resource Group
+	$resourceGroup = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location 
+
+	# Create Iot Central App
+	$tags = @{ $Tag1Key = $Tag1Value }
+	$newAzureIotCentralApp = New-AzureRmIotCentralApp -Name $IotCentralAppName -ResourceGroupName $ResourceGroupName -Subdomain $IotCentralSubdomain -Location $Location -Sku $Sku -Tag $tags -DisplayName $DisplayName
+
+	# Assert that there is only 1 Iot Central App in the RG
+	AssertNumberOfAppsInRG -ResourceGroupName $ResourceGroupName -count 1
+	
+	# Get Iot Central App
+	$iotCentralApp = Get-AzureRmIotCentralApp -ResourceGroupName $ResourceGroupName -Name $IotCentralAppName 
+
+	Assert-True { $iotCentralApp.Name -eq $IotCentralAppName }
+	Assert-True { $iotCentralApp.Sku -eq $Sku }
+	Assert-True { $iotCentralApp.Subdomain -eq $IotCentralSubdomain }
+	Assert-True { $iotCentralApp.DisplayName -eq $DisplayName }
+	Assert-True { $iotCentralApp.Location -eq $Location }
+	Assert-True { $iotCentralApp.Tag.Item($Tag1Key) -eq $Tag1Value}
+
+	# Update Display Name of Iot Central App, Test Piping
+	$NewDisplayName = "Updated Display Name"
+	$updatedIotCentralApp = Set-AzureRmIotCentralApp -Name $IotCentralAppName -ResourceGroupName $ResourceGroupName -DisplayName $NewDisplayName
+
+	# Confirm Display Name is updated
+	Assert-True { $updatedIotCentralApp.DisplayName -eq $DisplayName }
+
+	# Get the app, confirm change is propogated
+	$updatedIotCentralApp = Get-AzureRmIotCentralApp -Name $IotCentralAppName -ResourceGroupName $ResourceGroupName
+	Assert-True { $updatedIotCentralApp.DisplayName -eq $DisplayName }
+
+	# Change Tags
+	$tags = @{ $Tag2Key = $Tag2Value }
+	$updatedIotCentralApp = Set-AzureRmIotCentralApp -Name $IotCentralAppName -ResourceGroupName $ResourceGroupName -Tag $tags
+	Assert-True { $updatedIotCentralApp.Tag.Count -eq 1 }
+	Assert-True { $updatedIotCentralApp.Tag.Item($Tag2Key) -eq $Tag2Value }
+
+	# Remove IotCentral App
+	Remove-AzureRmIotCentralApp -ResourceGroupName $ResourceGroupName -Name $IotCentralAppName
+
+	AssertNumberOfAppsInRG -ResourceGroupName $ResourceGroupName -count 0
+
+	# Remove Resource Group
+	Remove-AzureRmResourceGroup $ResourceGroupName
+}
+
+function GetNumberElementsInResourceGroup{
+	Param($ResourceGroupName)
+	$iotCentralAppsInRG = Get-AzureRmIotCentralApp -ResourceGroupName $ResourceGroupName
+	return $iotCentralAppsInRG.Count
+}
+
+function AssertNumberOfAppsInRG{
+	Param($ResourceGroupName,$count)
+	$countInRG = GetNumberElementsInResourceGroup -ResourceGroupName $ResourceGroupName
+	Assert-True($countInRG -eq $count)
+}
